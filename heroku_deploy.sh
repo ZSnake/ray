053ded408e02e0cc4c11109c241fe66cfff388b5
Viktor Zavala <viktor@acklenavenue.com>
2017-10-06 00:31:10 -0600

    added first step to deploy

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -e
 APP_NAME=$1
 
+
+git remote add heroku git@heroku.com:$APP_NAME.git
+git fetch heroku
+
 heroku maintenance:on --app $APP_NAME
 git push subtree push --prefix dist heroku $CIRCLE_SHA1:refs/heads/master
 heroku restart --app $APP_NAME
