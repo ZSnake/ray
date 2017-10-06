d2d1a61cd1d7e10c7a0977014ce786e2e83db89c
Viktor Zavala <viktor@acklenavenue.com>
2017-10-06 00:15:35 -0600

    changing deploy command and circle.yml file

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -2,6 +2,6 @@
 APP_NAME=$1
 
 heroku maintenance:on --app $APP_NAME
-git push heroku $CIRCLE_SHA1:refs/heads/master
+git push subtree push --prefix dist heroku $CIRCLE_SHA1:refs/heads/master
 heroku restart --app $APP_NAME
 heroku maintenance:off --app $APP_NAME
